d9736b5817075170eea8f06c66c22aff7459a0fc
kumavis <aaron@kumavis.me>
2017-11-25 14:31:56 -1000

    add dino

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 console.log('hello')
 
+const typeTarget = 'galileo'.split('')
+
+
 const input = document.querySelector('input')
 input.focus()
 
 const display = document.querySelector('#display')
 window.display = display
 
+updateDisplay()
+
 input.addEventListener('input', (event) => {
-  const character = event.data
-  display.textContent = character
-  console.log(character)
+  console.log('hi!')
+  const char = event.data
+  console.log(char)
+  processType(char)
 })
+
+function processType(char){
+  const nextTarget = typeTarget[0]
+  if (nextTarget === char) typeTarget.shift()
+  updateDisplay()
+}
+
+function updateDisplay(){
+  console.log('update display')
+  display.textContent = typeTarget.join('')
+}
